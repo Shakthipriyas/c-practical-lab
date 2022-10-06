f73bdcccaae1d0ac9ebbb4a24d45861bdea286cb
Shakthipriyas <112743584+Shakthipriyas@users.noreply.github.com>
2022-10-06 13:17:45 +0530

    Add files via upload

---
+++ day1_ex.p1_greatest of three numbers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cout<<"enter first no:";
+	cin>>a;
+	while(a,b,c)
+	{
+		if(a>=b and a>=c)
+		cout<<"greatest is A";
+		cin>>a;
+	}
+}
